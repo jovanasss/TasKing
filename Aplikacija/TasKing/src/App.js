9c3eaec4f0813eafe550aa15da6c88759f33afa8
ivkoneli <shadowmoister@gmail.com>
2022-05-07 21:59:19 +0200

    Login page start

--- Aplikacija/TasKing/src/App.js
+++ Aplikacija/TasKing/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import * as React from 'react';
+
 import './App.css';
+import LoginForm from './components/LoginForm';
 
 function App() {
+
+
+  /* ovde treba da bude klasa korisnik da bi se isprobao login 
+
+  const adminUser = {
+    userName : "admin",
+    password : "admin"
+  }
+  */
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <LoginForm/>
     </div>
   );
 }
 
 export default App;
+
---
+++ Aplikacija/TasKing/src/components/LoginForm.js
@@ -0,0 +1,88 @@
+import React, { Component }  from "react";
+import Checkbox from '@mui/material/Checkbox';
+import { pink , orange } from "@mui/material/colors";
+import { FormControlLabel, TextField } from "@mui/material";
+import {ThemeProvider} from "@mui/system";
+import { createTheme , experimental_sx as sx} from "@mui/material/styles"
+
+
+
+function LoginForm()  {
+
+
+  // kreiranje teme u MUI ( bez toga nije htela da se promeni boja elementa)
+    const theme = createTheme({
+        components:{
+          MuiTextField : {styleOverrides:{
+              root : sx ({
+                "& .MuiOutlinedInput-root": {
+                    "& > fieldset": {
+                      borderColor: "rgb(161, 17, 161)",
+                    },
+                ":hover"  :{
+                    "& > fieldset": {
+                        borderColor: "rgb(161, 17, 161)",
+                      },
+                }  
+                }
+              })
+          }}  
+        },
+        palette: {
+          primary: {
+            main: "rgb(161, 17, 161)",
+          },
+          secondary:{
+            main : pink[100],
+          }
+        },
+      });
+
+
+
+
+
+return (
+    <div className="divMain">
+
+        <form className="Forma">
+            <label className="labelLogin">LOGIN</label>
+
+            <div className="divUser">
+                  /-- element na koji hocemo da primenimo temu mora da se wrapuje  //
+                    <ThemeProvider theme={theme} >
+                    <TextField id="outlined-basic" label="Username" variant="outlined" type="text" color="primary"/>
+                    </ThemeProvider>
+            </div>
+
+            <div className="divPass">
+                    <ThemeProvider theme={theme} >
+                    <TextField id="outlined-basic" label="Password" variant="outlined" type="password" color="primary"/>
+                    </ThemeProvider>
+            </div>
+
+            <div className="divCheck">
+                    <FormControlLabel
+                        label = "Remember me ?"
+                        control = {
+                            <Checkbox                    
+                                sx={{
+                                    color: "rgb(161, 17, 161)",
+                                    '&.Mui-checked': {
+                                    color: "rgb(161, 17, 161)",
+                                    },
+                                }}
+                            />
+                        }
+                    />
+            </div>
+
+            <button className="BtnLogin">LOGIN</button>
+
+            <label className="OrSignUp">Need an account ? <a href ="google.rs" >SignUp</a></label>   
+        </form>
+    </div>
+)
+}
+
+export default LoginForm ;
